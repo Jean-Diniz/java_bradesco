7ac5a9f1fe8d4af4e05ba0f16c3fee097d6fa247
Jean Diniz <jean.diniz@youxgroup.com.br>
2023-05-25 10:51:03 -0300

    Simple editor and save

--- src/com/bradesco/curso/frame/Frame.java
+++ src/com/bradesco/curso/frame/Frame.java
@@ -1,18 +1,76 @@
 package com.bradesco.curso.frame;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements ActionListener {
+
+    private JTextArea textArea;
 
     public Frame() {
 
-        super("New frame");
+        super("Editor");
 
-        JButton button = new JButton("Click");
+        Container container = getContentPane();
 
-        getContentPane().add(button);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        JButton exit = new JButton("Exit");
+        JButton save = new JButton("Save");
+        textArea = new JTextArea();
+
+        exit.addActionListener(this);
+        save.addActionListener(this);
+
+        btnPanel.add(save);
+        btnPanel.add(exit);
+
+        container.add(textArea);
+        container.add(BorderLayout.SOUTH, btnPanel);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(400, 400);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Exit" -> dispose();
+            case "Save" -> {
+                try {
+                    save(textArea.getText());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void save(String text) throws IOException {
+        createOutputDir();
+
+        try (FileWriter fileWriter = new FileWriter("output/" + Timestamp.valueOf(LocalDateTime.now()) + ".txt");
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)
+        ) {
+            bufferedWriter.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createOutputDir() {
+        File output = new File("output");
+        if (!output.exists()) {
+            output.mkdir();
+        }
     }
 }
